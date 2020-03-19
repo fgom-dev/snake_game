4c55ec0db0c9fcf7eb49d4180724589f33b13b2a
fgom-dev <fnd.gomes02@gmail.com>
2020-03-19 08:52:15 -0300

    Frutinha definida.

--- snake.py
+++ snake.py
@@ -1,5 +1,7 @@
 import pygame
 
+from random import randint
+
 pygame.init()
 
 resolucao = (500, 500)
@@ -8,7 +10,25 @@ preto = (0, 0, 0)
 screen.fill(preto)
 pygame.display.update()
 
+
+class Frutinha:
+    cor = (255, 0, 0)
+    tamanho = (10, 10)
+
+    def __init__(self):
+        self.textura = pygame.Surface(self.tamanho)
+        self.textura.fill(self.cor)
+        x = randint(0, 49) * 10
+        y = x
+        self.posicao = (x, y)
+
+
+frutinha = Frutinha()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+
+    screen.blit(frutinha.textura, frutinha.posicao)
+    pygame.display.update()
