3bad78447346a89682c8aeb6f820ad849d5959ad
fgom-dev <fnd.gomes02@gmail.com>
2020-03-19 10:32:40 -0300

    Movimentação do snake feita

--- snake.py
+++ snake.py
@@ -1,19 +1,19 @@
-import pygame
+import random
 
-from random import randint
+import pygame
 
 pygame.init()
 
 resolucao = (500, 500)
 screen = pygame.display.set_mode(resolucao)
 preto = (0, 0, 0)
-screen.fill(preto)
-pygame.display.update()
+clock = pygame.time.Clock()
 
 
 class Snake:
     cor = (255, 255, 255)
     tamanho = (10, 10)
+    velocidade = 10
 
     def __init__(self):
         self.textura = pygame.Surface(self.tamanho)
@@ -21,9 +21,43 @@ class Snake:
 
         self.corpo = [(100, 100), (90, 100), (80, 100)]
 
-    def blit(self):
+        self.direcao = 'direita'
+
+    def blit(self, screen):
         for posicao in snake.corpo:
             screen.blit(self.textura, posicao)
+
+    def andar(self):
+        cabeca = self.corpo[0]
+        x = cabeca[0]
+        y = cabeca[1]
+
+        if self.direcao == 'direita':
+            self.corpo.insert(0, (x + self.velocidade, y))
+        elif self.direcao == 'esquerda':
+            self.corpo.insert(0, (x - self.velocidade, y))
+        elif self.direcao == 'cima':
+            self.corpo.insert(0, (x, y - self.velocidade))
+        elif self.direcao == 'baixo':
+            self.corpo.insert(0, (x, y + self.velocidade))
+
+        self.corpo.pop(-1)
+
+    def cima(self):
+        if self.direcao != 'baixo':
+            self.direcao = 'cima'
+
+    def baixo(self):
+        if self.direcao != 'cima':
+            self.direcao = 'baixo'
+
+    def esquerda(self):
+        if self.direcao != 'direita':
+            self.direcao = 'esquerda'
+
+    def direita(self):
+        if self.direcao != 'esquerda':
+            self.direcao = 'direita'
 
 
 class Frutinha:
@@ -33,11 +67,11 @@ class Frutinha:
     def __init__(self):
         self.textura = pygame.Surface(self.tamanho)
         self.textura.fill(self.cor)
-        x = randint(0, 49) * 10
+        x = random.randint(0, 49) * 10
         y = x
         self.posicao = (x, y)
 
-    def blit(self):
+    def blit(self, screen):
         screen.blit(self.textura, self.posicao)
 
 
@@ -45,11 +79,30 @@ frutinha = Frutinha()
 snake = Snake()
 
 while True:
+    clock.tick(15)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
-    frutinha.blit()
-    snake.blit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.cima()
+                break
+            elif event.key == pygame.K_DOWN:
+                snake.baixo()
+                break
+            elif event.key == pygame.K_LEFT:
+                snake.esquerda()
+                break
+            elif event.key == pygame.K_RIGHT:
+                snake.direita()
+                break
+
+    snake.andar()
+
+    screen.fill(preto)
+    frutinha.blit(screen)
+    snake.blit(screen)
+
 
     pygame.display.update()
